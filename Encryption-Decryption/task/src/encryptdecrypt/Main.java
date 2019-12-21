5ad2a0018cc751260b0c9194e8b1c61f59294f5b
Zeegit <sergey.zemskov@gmail.com>
2019-12-21 18:48:35 +0300

    stage 2

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,24 +1,30 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //
         String direct  = "abcdefghijklmnopqrstuvwxyz";
-        String reverse = "zyxwvutsrqponmlkjihgfedcba";
+        //String reverse = "zyxwvutsrqponmlkjihgfedcba";
 
-        String textDirect = "we found a treasure!";
-        String textReverse = "";
+        String textDirect = scanner.nextLine();
+        int offset = Integer.parseInt(scanner.nextLine());
+
+        String textEncryption = "";
         int pos;
         for(int i = 0; i < textDirect.length(); i++) {
             pos = direct.indexOf(textDirect.charAt(i));
             if (pos > -1) {
-                textReverse += reverse.charAt(pos);
+                int x = (pos+offset) % direct.length();
+                textEncryption += direct.charAt(x);
             } else {
-                textReverse += textDirect.charAt(i);
+                textEncryption += textDirect.charAt(i);
             }
         }
 
-        System.out.println(textReverse);
+        System.out.println(textEncryption);
 
     }
 }
