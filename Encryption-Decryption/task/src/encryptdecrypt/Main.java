a148618a9dd3db5feb20752b11476b4198b5fb3a
Zeegit <sergey.zemskov@gmail.com>
2019-12-21 13:39:20 +0300

    stage 1

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,6 +2,23 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //
+        String direct  = "abcdefghijklmnopqrstuvwxyz";
+        String reverse = "zyxwvutsrqponmlkjihgfedcba";
+
+        String textDirect = "we found a treasure!";
+        String textReverse = "";
+        int pos;
+        for(int i = 0; i < textDirect.length(); i++) {
+            pos = direct.indexOf(textDirect.charAt(i));
+            if (pos > -1) {
+                textReverse += reverse.charAt(pos);
+            } else {
+                textReverse += textDirect.charAt(i);
+            }
+        }
+
+        System.out.println(textReverse);
+
     }
 }
